f8365e49f6ce0692ee9bb37c421ca798a2215353
ryansurf <94500732+ryansurf@users.noreply.github.com>
2022-06-12 00:28:36 -0700

    Update Helium_Scrape.py

--- Helium_Scrape.py
+++ Helium_Scrape.py
@@ -1,7 +1,11 @@
 import requests
 
 def check_status():
-    url = "https://api.helium.io/v1/hotspots/11CSjJtJRWK6ZT16KcRK2Hy51gMBPgLvJMAuFsEnyWgd1qwcucW"
+    #The url needs your miners address. This can be found on the explorer page, and is the string followed
+    #by the "@" symb
+    url = "https://api.helium.io/v1/hotspots/INSERT_YOUR_ADDRESS_HERE
+    #Example
+    #url = "https://api.helium.io/v1/hotspots/112Axn3a3kDZFkuWgbQTsNvwPH7eK8iM3RBrMwGVxiPT9YK6hDt4"
     r = requests.get(url)
 
     json_data = r.json()
